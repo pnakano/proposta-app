fdaf0e1d8aa5a9b8f6cb155c928ffeca30a79003
Pamela Nakano <pamela.nakano@hotmail.com>
2024-08-14 15:46:35 -0300

    Criado services

---
+++ src/main/java/com/pamelanakano/proposta_app/service/PropostaService.java
@@ -0,0 +1,10 @@
+package com.pamelanakano.proposta_app.service;
+
+import com.pamelanakano.proposta_app.http.dto.PropostaRequestDto;
+import com.pamelanakano.proposta_app.http.dto.PropostaResponseDto;
+
+public interface PropostaService {
+
+    PropostaResponseDto save(PropostaRequestDto requestDto);
+
+}
---
+++ src/main/java/com/pamelanakano/proposta_app/service/PropostaServiceImpl.java
@@ -0,0 +1,15 @@
+package com.pamelanakano.proposta_app.service;
+
+import com.pamelanakano.proposta_app.http.dto.PropostaRequestDto;
+import com.pamelanakano.proposta_app.http.dto.PropostaResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropostaServiceImpl implements PropostaService {
+
+   @Override
+   public PropostaResponseDto save(PropostaRequestDto requestDto) {
+       return null;
+   }
+
+}
