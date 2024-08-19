3c110bbff33ffa22e096211961c0162ff7843c61
Pamela Nakano <pamela.nakano@hotmail.com>
2024-08-19 17:50:34 -0300

    Correção de importação

--- src/main/java/com/pamelanakano/proposta_app/scheduler/PropostaSemIntegracao.java
+++ src/main/java/com/pamelanakano/proposta_app/scheduler/PropostaSemIntegracao.java
@@ -5,7 +5,6 @@ import com.pamelanakano.proposta_app.repository.PropostaRepository;
 import com.pamelanakano.proposta_app.service.NotificacaoRabbitService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
