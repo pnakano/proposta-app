37b9e46b9bb1b946f58a25441eb3f34c1b73306f
Pamela Nakano <pamela.nakano@hotmail.com>
2024-08-20 15:46:37 -0300

    Implementação do WebSocket

--- src/main/java/com/pamelanakano/proposta_app/listener/PropostaConcluidaListener.java
+++ src/main/java/com/pamelanakano/proposta_app/listener/PropostaConcluidaListener.java
@@ -1,7 +1,10 @@
 package com.pamelanakano.proposta_app.listener;
 
+import com.pamelanakano.proposta_app.http.dto.PropostaResponseDto;
+import com.pamelanakano.proposta_app.mapper.PropostaMapper;
 import com.pamelanakano.proposta_app.model.Proposta;
 import com.pamelanakano.proposta_app.repository.PropostaRepository;
+import com.pamelanakano.proposta_app.service.interfaces.WebSocketService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +13,18 @@ public class PropostaConcluidaListener {
 
     private final PropostaRepository propostaRepository;
 
-    public PropostaConcluidaListener(PropostaRepository propostaRepository) {
+    private final WebSocketService webSocketService;
+
+    public PropostaConcluidaListener(PropostaRepository propostaRepository, WebSocketService webSocketService) {
         this.propostaRepository = propostaRepository;
+        this.webSocketService = webSocketService;
     }
 
     @RabbitListener(queues = "${rabbitmq.propostaconcluida.proposta}")
     public void propostaConcluida(Proposta proposta) {
-        propostaRepository.save(proposta);
+        propostaRepository.updateProposta(proposta.getId(), proposta.getAprovada(), proposta.getObservacao());
+        PropostaResponseDto responseDto = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+        webSocketService.notificar(responseDto);
     }
 
 }
