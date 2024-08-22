f837c1fb0530f41bfade51fac1702087dfbbcaad
Pamela Nakano <pamela.nakano@hotmail.com>
2024-08-22 17:40:30 -0300

    Implementação do spring doc

--- src/main/java/com/pamelanakano/proposta_app/http/PropostaController.java
+++ src/main/java/com/pamelanakano/proposta_app/http/PropostaController.java
@@ -2,6 +2,7 @@ package com.pamelanakano.proposta_app.http;
 
 import com.pamelanakano.proposta_app.http.dto.PropostaRequestDto;
 import com.pamelanakano.proposta_app.http.dto.PropostaResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +12,11 @@ import java.util.List;
 
 public interface PropostaController {
 
+    @Operation(summary = "Salva a proposta de acordo com o JSON passado por parâmetro")
     @PostMapping
     ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto propostaRequestDto);
 
+    @Operation(summary = "Retorna todas as propostas cadastradas")
     @GetMapping
     ResponseEntity<List<PropostaResponseDto>> listarPropostas();
 
