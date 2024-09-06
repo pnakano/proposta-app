23e44f0d1f669ac2b8d8eea980c616011ab49545
Pamela Nakano <pamela.nakano@hotmail.com>
2024-09-06 19:12:09 -0300

    Ajuste no CORS para adequação ao projeto front-end

--- src/main/java/com/pamelanakano/proposta_app/config/CorsConfiguration.java
+++ src/main/java/com/pamelanakano/proposta_app/config/CorsConfiguration.java
@@ -10,8 +10,9 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost/")
-                .allowedMethods("*");
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 
 }
